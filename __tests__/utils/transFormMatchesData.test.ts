76d13a47bc9e56f874044b72cacea4fe17c6b2a5
Denno31 <denniskyn80@gmail.com>
2024-02-03 16:21:46 +0300

    added more tests

---
+++ __tests__/utils/transFormMatchesData.test.ts
@@ -0,0 +1,24 @@
+import { Match } from "@/types/types";
+import { transFormMatchesData } from "@/utils/getMatchesData";
+import matchesData from "@/data/sports.json";
+
+const mockMatches: Match[] = [matchesData[0]];
+
+test("transFormMatchesData transforms matches data correctly", () => {
+  const transformedData = transFormMatchesData(mockMatches);
+
+  expect(transformedData).toHaveLength(mockMatches.length);
+
+  expect(transformedData[0]).toEqual({
+    homeTeam: { name: "FK Tyumen", score: 0 },
+    awayTeam: { name: "Luch-Energiya Vladivostok", score: 0 },
+    country: "Russia",
+    timestamp: 1470484800,
+    id: "hbaffaf",
+    competition: "Football National League",
+    status: { type: "finished", label: "ENDED" },
+    liveStatus: "FT",
+  });
+
+  // Add more assertions or test cases based on your specific requirements
+});
